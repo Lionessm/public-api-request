2513ad51ddd3ee7816d68c2632c3e66fa33168be
Maria <maria.mihaila.17@gmail.com>
2020-10-15 22:30:50 +0300

    Create function modalEmployee(i) for current clicked employee modal markup, appending it to body and removing it from body; Function modalEmployee(i) is called if the employee container is clicked; Add event listener for exit button and remove markup from body.

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-
+let allEmployees;
 // - Fetch 12 random users using randomuser.me api
 
 fetch('https://randomuser.me/api/?results=12')
     .then(res => res.json())
     .then(data => {
-        console.log(data.results)
+
+        allEmployees = data.results;
+        console.log('allEmployees ' , allEmployees);
+
         for (let i = 0; i < data.results.length; i++) {
-            createEmployeesMarkup(data.results[i]);
+            createEmployeesMarkup(data.results[i], i);
+            addEventListenerToEmployee(i);
         }
     })
-
 
 // Creates gallery containers for every employee and displays their image, name, email, city and country.
 // Appends this markup with innerHTML
 
-function createEmployeesMarkup(employee) {
+function createEmployeesMarkup(employee, i) {
     const markup =
-    `<div class="card">
+    `<div class="card" id="employee_id_${i}">
         <div class="card-img-container">
             <img class="card-img" src="${employee['picture']['large']}" alt="profile picture">
         </div>
@@ -29,15 +32,47 @@ function createEmployeesMarkup(employee) {
 
     const gallery = document.getElementById('gallery');
     gallery.insertAdjacentHTML('beforeend', markup);
-    return markup;
 }
 
-document.addEventListener('click',function(e){
-    console.log(e.target.className);
-    if(e.target && e.target.className === 'card'){
-        console.log("I work");
-    }
-})
+
+function addEventListenerToEmployee(i) {
+    let container = document.getElementById(`employee_id_${i}`);
+    container.addEventListener('click' , (e) => {
+        console.log("am intrat in modal employee");
+        modalEmployee(i);
+    })
+};
+
+function modalEmployee(i) {
+    let clickedEmployeeModalDisplay =
+        `<div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${allEmployees[i]['picture']['large']}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${allEmployees[i]['name']['first']} ${allEmployees[i]['name']['last']}</h3>
+                        <p class="modal-text">${allEmployees[i]['email']}</p>
+                        <p class="modal-text cap">${allEmployees[i]['location']['city']}</p>
+                        <hr>
+                            <p class="modal-text">${allEmployees[i]['phone']}</p>
+                            <p class="modal-text">${allEmployees[i]['location']['street']['number']}, ${allEmployees[i]['location']['street']['name']}, ${allEmployees[i]['location']['state']}, ${allEmployees[i]['location']['postcode']}, ${allEmployees[i]['location']['country']}</p>
+                            <p class="modal-text">Birthday: ${allEmployees[i]['dob']['date']}</p>
+                </div>
+            </div>`
+
+    const body = document.getElementsByTagName("body")[0];
+    body.insertAdjacentHTML('beforeend', clickedEmployeeModalDisplay);
+
+    const exitButton = document.getElementById("modal-close-btn");
+    exitButton.addEventListener('click' , (e) => {
+
+        console.log("T!")
+
+        const myModalWindow = document.getElementsByClassName("modal-container")[0];
+        body.removeChild(myModalWindow);
+    })
+}
+
 
 
 
@@ -49,3 +84,6 @@ document.addEventListener('click',function(e){
 // - Append the built html markup into the gallery div
 // - Add event listener for each markup on click
 // - On event highlight the profile that was selected
+
+// - Store in a global variable the employees list
+// - Create modal markup for emplyee
